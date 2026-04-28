b00842b101ea0c150967fe9bb8e4b639cd1efea3
nour eldin mahmoud <https://github.com/Nour5Eldin>
2026-04-28 09:18:23 +0300

    switch to static output

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideCloudinaryLoader } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()) 
+    provideHttpClient(withFetch()),
+    provideCloudinaryLoader('https://res.cloudinary.com/dt5u0fgqz/image/upload')
   ]
 };
